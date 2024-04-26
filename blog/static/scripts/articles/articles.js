5d7c4c862007f64cf90f91a91cc2093f86d1644c
Sameh ElGazar <selgazar95@gmail.com>
2024-04-26 19:47:29 +0300

    Merge from dev, articles

--- blog/static/scripts/articles/articles.js
+++ blog/static/scripts/articles/articles.js
@@ -1,28 +1,35 @@
 $(document).ready(function () {
-  
   $.ajax({
     method: 'GET',
     url: 'http://localhost:5001/blog/api/v1/posts/',
     contentType: 'application/json',
     success: function (data) {
-      data.forEach(post => {
+      data.forEach((post) => {
         if (post.tag === undefined) {
           post.tag = '';
         }
         $('.container').append(`
           <article class="post">
             <div class="article_img">
-              <a href="http://localhost:5000/post/${post.id}" style="background-image: url('${post.cover}')"></a>
+              <a href="http://localhost:5000/post/${
+                post.id
+              }" style="background-image: url('${post.cover}')"></a>
             </div>
             <div class="article_data">
-              <a href="http://localhost:5000/post/${post.id}" class="article_header">${post.title.charAt(0).toUpperCase() + post.title.slice(1)}</a>
+              <a href="http://localhost:5000/post/${
+                post.id
+              }" class="article_header">${
+          post.title.charAt(0).toUpperCase() + post.title.slice(1)
+        }</a>
             </div>
           </article>`);
 
         $('article').hover(
           function () {
             if ($(this).find('p').length === 0) {
-              $(this).find('div.article_img a').append(`<p class="hover-tag">${post.tag}</p>`);
+              $(this)
+                .find('div.article_img a')
+                .append(`<p class="hover-tag">${post.tag}</p>`);
             }
           },
           function () {
@@ -30,6 +37,6 @@ $(document).ready(function () {
           }
         );
       });
-    }
+    },
   });
 });
