4b015392005a64acf77f9d72f4227e831246ab32
nono <nour98nour@gmail.com>
2024-04-26 17:26:35 +0300

    post CRUD but without D

--- blog/routes/routes.py
+++ blog/routes/routes.py
@@ -58,12 +58,12 @@ def articles():
     return render_template('articles.html')
 
 
-def save_picture(form_picture):
+def save_picture(form_picture, w, h):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/images', picture_fn)
-    output_size = (150, 150)
+    output_size = (w, h)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
     i.save(picture_path)
@@ -78,7 +78,7 @@ def account():
         current_user.email = form.email.data
         current_user.username = form.username.data
         if form.image.data:
-            current_user.avatar = save_picture(form.image.data)
+            current_user.avatar = save_picture(form.image.data, 150, 150)
         db.session.commit()
         return redirect(url_for('account'))
     elif request.method == 'GET':
@@ -90,8 +90,11 @@ def account():
 
 def create_tags_post(post, tags_in_post):
     for tag in str(tags_in_post).split():
-        t = Tag(name=tag)
-        db.session.add(t)
+        if not Tag.query.filter_by(name=tag).first():
+            t = Tag(name=tag)
+            db.session.add(t)
+        else:
+            t = Tag.query.filter_by(name=tag).first()
         post.tags.append(t)
     db.session.commit()
 
@@ -102,7 +105,7 @@ def create_post():
     form = CreateAndUpdatePostForm()
     if form.validate_on_submit():
         if form.post_image.data:
-            cover = save_picture(form.post_image.data)
+            cover = save_picture(form.post_image.data, 800, 600)
         else:
             cover = 'https://via.placeholder.com/800x400'
         p = Post(title=form.title.data, content=form.content.data, cover=cover, user_id=current_user.id)
@@ -115,19 +118,36 @@ def create_post():
 
 
 @app.route('/post/<int:post_id>')
-@login_required
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     tags = post.tags
     return render_template('post.html', title='post', post=post, tags=tags)
 
 
-@app.route('/post/<int:post_id>/update')
-@login_required
-def update_post(post_id):
-    post = Post.query.get_or_404(post_id)
-    tags = post.tags
-    return render_template('post.html', title='post', post=post, tags=tags)
+# @app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+# @login_required
+# def update_post(post_id):
+#     post = Post.query.get_or_404(post_id)
+#     if current_user.id != post.user_id:
+#         abort(403)
+#     form = CreateAndUpdatePostForm()
+#     if form.validate_on_submit():
+#         if form.post_image.data:
+#             cover = save_picture(form.post_image.data, 800, 600)
+#             post.cover = cover
+#         post.title = form.title.data
+#         post.content = form.content.data
+#         for t in post.tags:
+#             db.session.query(post_tag).filter(post_tag.c.tag_id == t.id).delete()
+#         if form.tags.data:
+#             create_tags_post(post, form.tags.data)
+#         db.session.commit()
+#         return redirect(url_for('home'))
+#     form.content.data = post.content
+#     form.title.data = post.title
+#     form.tags.data = ' '.join([tag.name for tag in post.tags])
+#     tags = post.tags
+#     return render_template('new_post.html', title='post', form=form)
 
 
 @app.route('/post/<int:post_id>/delete')
